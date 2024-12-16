11d066e941c3a250058c7b821ce1761397185523
jay25102 <jasonhuang25102@gmail.com>
2024-12-15 19:50:17 -0500

    even more notes

---
+++ app/posts/error.js
@@ -0,0 +1,5 @@
+/**
+ * errors are rendered to the user with an error.js file
+ * note that these error pages MUSt be client side to
+ * recognize errors and present them in a meaningful way
+ */
--- app/posts/page.js
+++ app/posts/page.js
@@ -2,4 +2,18 @@
  * Routing in Next.js works as a file system.
  * This page.js represents localhost:3000/posts.
  * 
+ * 
+ * next.js gives us three options for data fetching
+ * 1. server side rendering (SSR)
+ * 2. Static site generation (SSG)
+ * 3. incremental static generation (ISR)
+ * 
+ * by default next.js uses static site generation, which caches
+ * the page in the web browser
+ * 
+ * server side will always be up to date with the server
+ * 
+ * incremental is inbetween, where it will fetch new info from
+ * the server with a set amount of time in between each fetch,
+ * and otherwise will store in the browser
  */
